c38e96d5975e7c1667abd1b3c67be70d548f9f89
AndresLopez420 <73365005+AndresLopez420@users.noreply.github.com>
2022-12-04 22:14:06 -0300

    cambios al listado de horas

--- Prototipo/src/routes/paciente.js
+++ Prototipo/src/routes/paciente.js
@@ -6,7 +6,7 @@ router.get('/formulariop', async (req,res) => {
     res.render('paciente/formulariop');
 });
 
-router.post('/add', async (req,res) => {
+router.post('/formualariop', async (req,res) => {
     const {rut, nombres, apellidos, telefono_personal, telefono_contacto, sexo, talla, fecha_nacimiento, correo_electronico, direccion} = req.body;
     const nuevaSolicitud = {
         rut, nombres, apellidos, telefono_personal, telefono_contacto, sexo, talla, fecha_nacimiento, correo_electronico, direccion
@@ -16,8 +16,8 @@ router.post('/add', async (req,res) => {
 });
 
 router.get('/listap', async (req,res) => {
-    const horas = await pool.query("SELECT id_hora , disponibilidad , DATE_FORMAT(fecha_hora, '%Y-%m-%d %H:%i:%s.') as 'horario', nombre FROM hora_medica INNER JOIN profesional ON hora_medica.rut_p = profesional.rut_p");
-    res.render('paciente/listap', { horas });
+    const horas = await pool.query("SELECT profesional.nombre, profesional.num_sala, especialidad.especialidad, hora_medica.fecha_hora, hora_medica.disponibilidad FROM profesional INNER JOIN pro_espec ON profesional.rut_p = pro_espec.rut_p INNER JOIN especialidad ON pro_espec.id_especialidad = especialidad.id_especialidad INNER JOIN hora_medica ON hora_medica.rut_p = profesional.rut_p;");
+    res.render('paciente/listap', {  horas });
 });
 
 router.get('/solicitud', (req,res) => {
