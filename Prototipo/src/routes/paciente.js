814f0d78c1e8d1e0f334a3bee0d5bf38551c5bb9
AndresLopez420 <andres.lopez@alumnos.uv.cl>
2022-12-07 08:05:06 -0300

    Prototipo final

--- Prototipo/src/routes/paciente.js
+++ Prototipo/src/routes/paciente.js
@@ -4,55 +4,50 @@ const router = express.Router();
 
 const pool = require('../database');
 
-router.get('/formulariop', async (req,res) => {
-    res.render('paciente/formulariop');
+router.get('/formulariop/:id_h', async (req,res) => {
+    const valor = [
+        {id: req.params.id_h}
+    ]
+    res.render('paciente/formulariop',{valor});
 });
 
-router.post('/formulariop', async (req,res) => {
+router.post('/formulariop/:id', async (req,res) => {
     const {rut, nombres, apellidos, telefono_personal, telefono_contacto, sexo, talla, fecha_nacimiento, correo_electronico, direccion} = req.body;
-    const nuevaPaciente = {
+    const nuevoPaciente = {
         rut, 
         nombres, 
         apellidos, 
         telefono_personal, 
         telefono_contacto, 
-        sexo, talla, 
+        sexo, 
+        talla, 
         fecha_nacimiento, 
         correo_electronico, 
         direccion
     };
-    await pool.query('INSERT INTO persona set ?', [nuevaPaciente]);
+    const id = req.params.id;
+    await pool.query('INSERT INTO persona set ?', [nuevoPaciente]);
     req.flash('success','Paciente ingresado satisfactoriamente');
-    res.redirect('solicitud/${rut}');
+    //res.redirect('../../paciente/solicitud/:id');
+    res.render('paciente/solicitud',{nuevoPaciente,id});
 });
 
 router.get('/listap', async (req,res) => {
-    const horas = await pool.query("SELECT profesional.nombre, profesional.num_sala, especialidad.especialidad, DATE_FORMAT(hora_medica.fecha_hora, '%Y-%m-%d %H:%i:%s.') as 'horario', hora_medica.disponibilidad FROM profesional INNER JOIN pro_espec ON profesional.rut_p = pro_espec.rut_p INNER JOIN especialidad ON pro_espec.id_especialidad = especialidad.id_especialidad INNER JOIN hora_medica ON hora_medica.rut_p = profesional.rut_p;");
+    const horas = await pool.query("SELECT hora_medica.id_hora, profesional.nombre, profesional.num_sala, especialidad.especialidad, DATE_FORMAT(hora_medica.fecha_hora, '%Y-%m-%d %H:%i:%s.') as 'horario', hora_medica.disponibilidad FROM profesional INNER JOIN pro_espec ON profesional.rut_p = pro_espec.rut_p INNER JOIN especialidad ON pro_espec.id_especialidad = especialidad.id_especialidad INNER JOIN hora_medica ON hora_medica.rut_p = profesional.rut_p;");
     res.render('paciente/listap', {  horas });
 });
 
-router.get('/solicitud/:rut', (req,res) => {
-    const { rut } = req.params;
-    res.render('paciente/solicitud/${rut}');
-});
-
-router.get('/eliminar/:id',async (req,res) => {
-    const { id } = req.params;
-    await pool.query("DELETE FROM hora_medica WHERE id_hora = ?", [id]);
-    req.flash('success','Hora medica eliminada satisfactoriamente');
-    res.redirect('../lista');
-});
-
-router.post('/solicitud/', async (req,res) => {
-    const rut = await pool.query("SELECT rut from persona");
-    const {motivo} = req.body;
+router.post('/solicitud/:rut', async (req,res) => {
+    const {motivo,id_hora} = req.body;
+    let rut = req.params.rut;
     const nuevaSolicitud = {
-        rut,
-        motivo
-    };  
-    await pool.query("INSERT INTO solicita SET ? ",[nuevaSolicitud]);
+        motivo,
+        id_hora,
+        rut
+    };
+    await pool.query("INSERT INTO solicita SET ? ", [nuevaSolicitud]);
     req.flash('success','Se ha enviado su solicitud, se le enviara un correo con la respuesta');
-    res.redirect('paciente/listap');
+    res.redirect('/paciente/listap');
 });
 
 module.exports = router;
